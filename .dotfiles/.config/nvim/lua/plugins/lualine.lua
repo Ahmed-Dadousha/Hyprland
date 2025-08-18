4bf771bff43980f3f3d0b82cf6e7d4dcdd1b570f
Ahmed_Dadosha <Bioshock44Matrix44@gmail.com>
2025-08-18 18:53:09 +0300

    Feat: Lua line macro recodring indicator

--- .dotfiles/.config/nvim/lua/plugins/lualine.lua
+++ .dotfiles/.config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,19 @@
-return{
+return {
     'nvim-lualine/lualine.nvim',
+      dependencies = {
+    -- display macro recording
+    { "yavorski/lualine-macro-recording.nvim" }
+  },
     config = function()
-        require('lualine').setup({
+            require('lualine').setup({
             options = {
-            theme = "tokyonight-storm"
+                theme = "tokyonight-storm"
+            },
+            sections = {
+                lualine_c = {
+                    "macro_recording", "%S" ,
                 }
+            }
         })
     end
 }
